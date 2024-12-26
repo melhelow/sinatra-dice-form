308c4904971c870b176282d179133c861f113e9c
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2024-12-26 09:49:54 +0000

    build the code and the forms, and the view, and worked in the functionality and make it works as expected

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:from_c)
+end
+
+get("/process_roll") do
+  @dice_num = params.fetch("dice").to_i
+  @sides_num = params.fetch("sides").to_i
+  @results = Array.new(@dice_num) { rand(1..@sides_num) }
+
+  erb(:results)
+end
+
+get("/process_again") do
+  @dice_num = params.fetch("dice").to_i
+  @sides_num = params.fetch("sides").to_i
+  @results = Array.new(@dice_num) { rand(1..@sides_num) }
+
+  erb(:results)
 end
